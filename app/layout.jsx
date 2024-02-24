479f31a6603c7aef46fa1e078c331f40fef2a72b
Cifer0101 <l.kakabadze2020@gmail.com>
2024-02-24 23:08:52 +0400

    Session provider wrapper

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "@/assets/styles/global.css"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
+import AuthProvider from "@/components/AuthProvider"
 
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
@@ -10,13 +11,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html>
-      <body lang="en">
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html>
+        <body lang="en">
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   )
 }
 
